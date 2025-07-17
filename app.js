d47dec49e1bbfe3f7b89d86e7cb602f1902d3f76
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-07-17 12:30:23 +0300

    Feat: Validate User Input
    Protects our app from bad input processing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
-const ai = require('./services/ai.js')
+const ai = require('./services/ai.js');
+const { body, validationResult } = require('express-validator')
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,6 +9,12 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const validateInput = [
+  body('input')
+    .trim()
+    .notEmpty()
+    .withMessage('Input can not be empty')
+]
 
 app.get('/', (req, res) => {
   res.json('Welcome')
@@ -17,7 +24,14 @@ app.post('/', (req, res) => {
   res.json(`Data received. Your name is ${req.body.name}`)
 })
 
-app.post('/ai', async (req, res) => {
+app.post('/ai', validateInput, async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      error: errors.array(),
+    })
+  }
   const input = req.body.input;
   const response = await ai.generateResponse(input);
   if(response) {
@@ -25,9 +39,10 @@ app.post('/ai', async (req, res) => {
   }
   return res.status(500).json({
     success: false,
-    message: 'An internal error occured.'
+    error: 'An internal error occured.'
   })
 })
+
 app.listen(PORT, () => {
   console.log(`The app is live on port: ${PORT}`)
 })
